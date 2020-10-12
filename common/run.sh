bd97e5d6877ecf5777a20337ff26c344f5b967c4
Jian Weng <jian.weng@ucla.edu>
2020-10-11 20:21:33 -0700

    aggressive rename

---
+++ common/run.sh
@@ -0,0 +1,17 @@
+if [ -z "$SBCONFIG" ]; then
+export SBCONFIG="$SS/ss-scheduler/configs/revel-1x1.sbmodel"
+fi
+
+if [ -z "$BACKCGRA"]; then
+export BACKCGRA=1 
+fi
+
+make clean
+timeout 120 make $1
+FU_FIFO_LEN=15                                     \
+gem5.opt $SS/gem5/configs/example/se.py            \
+--cpu-type=MinorCPU --l1d_size=16kB --l1d_assoc=16 \
+--l1i_size=16kB --l2_size=128kB --caches           \
+--ruby --num-cpus=8 --num-dirs=8 --network=simple  \
+--topology=Mesh_XY --mesh-rows=2                   \
+--cmd=./$1
---
+++ compile/02_vector_norm/_Z6kernelPd_dfg_0.dfg.h
@@ -0,0 +1,21 @@
+#ifndef ___Z6kernelPd_dfg_0_H__
+#define ___Z6kernelPd_dfg_0_H__
+#define P__Z6kernelPd_dfg_0_sub0_v0 14
+#define P__Z6kernelPd_dfg_0_sub0_v1 18
+#define P__Z6kernelPd_dfg_0_sub1_v0 1
+#define P__Z6kernelPd_dfg_0_sub1_v1 20
+#define P__Z6kernelPd_dfg_0_sub2_v0 19
+
+#define P__Z6kernelPd_dfg_0_sub0_v2 13
+#define P__Z6kernelPd_dfg_0_sub1_v3 12
+#define P__Z6kernelPd_dfg_0_sub2_v1 0
+
+// CAUTION: This is a serialization-based version
+// of the schedule.  (ie. cheating)  It is for simulation only.
+// corresponding dfg is in: _Z6kernelPd_dfg_0.*.json
+
+#define _Z6kernelPd_dfg_0_size 96
+
+char _Z6kernelPd_dfg_0_config[96] = "filename:_Z6kernelPd_dfg_0";
+
+#endif //_Z6kernelPd_dfg_0_H
