ad7a76668cb5beb4a5f09f4cc08ed503699288fc
Jian Weng <jian.weng@ucla.edu>
2020-10-14 02:26:34 +0000

    rename the path

--- common/run.sh
+++ common/run.sh
@@ -1,5 +1,5 @@
 if [ -z "$SBCONFIG" ]; then
-export SBCONFIG="$SS/ss-scheduler/configs/revel-1x1.sbmodel"
+export SBCONFIG="$SS/spatial-scheduler/configs/revel-1x1.sbmodel"
 fi
 
 if [ -z "$BACKCGRA"]; then
@@ -9,7 +9,7 @@ fi
 make clean
 timeout 120 make $1
 FU_FIFO_LEN=15                                     \
-gem5.opt $SS/gem5/configs/example/se.py            \
+gem5.opt $SS/dsa-gem5/configs/example/se.py        \
 --cpu-type=MinorCPU --l1d_size=16kB --l1d_assoc=16 \
 --l1i_size=16kB --l2_size=128kB --caches           \
 --ruby --num-cpus=8 --num-dirs=8 --network=simple  \
